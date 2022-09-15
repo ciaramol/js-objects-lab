4ac0e4c1f9cc537ecb5780852a032b59db4eeeea
Ciara Molloy <20089106@mail.wit.ie>
2022-09-15 12:39:15 +0100

    Arrays.

--- fundamentals.js
+++ fundamentals.js
@@ -1,5 +1,6 @@
 const aCar = {
     owner : "Joe Bloggs",
+    address : '3 Walkers Lane',
     type: {
       make: "Toyota",
       model: "Corolla",
@@ -10,7 +11,25 @@ const aCar = {
       code: "WD",
       number: "1058",
     },
+    previous_owners: [
+      {
+        name: "Pat Smith",
+        address: "1 Main Street",
+      },
+      {
+        name: "Sheila Dwyer",
+        address: "2 High Street",
+      },
+    ],
+  };
+
+  aCar.mileage ="10000";
+  aCar.colour = {
+    exterior: "red",
+    interior: "leather",
   };
   
   console.log(aCar.owner + ' drives a ' + aCar.type.make)
   console.log("Reg. = " + aCar.registration.year + "-" + aCar.registration.code + "-" + aCar.registration.number)
+  console.log("It is a " + aCar.colour.exterior + " car, " + aCar.mileage + " mileage, with " + aCar.colour.interior + " interior.")
+  console.log('First owner : ' + aCar.previous_owners[0].name + " - " + aCar.previous_owners[0].address)
